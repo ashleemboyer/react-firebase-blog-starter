c690d745318b0064016f59a4304cc747ad3ba5a1
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2019-08-26 19:45:55 -0400

    Improving readability

--- src/pages/post.js
+++ src/pages/post.js
@@ -5,13 +5,16 @@ const Post = ({ match }) => {
   const slug = match.params.slug;
   const postSlugs = ["my-first-blog-post", "my-second-blog-post"];
 
-  return postSlugs.indexOf(slug) !== -1 ? (
+  const postDoesNotExist = postSlugs.indexOf(slug) === -1;
+  if (postDoesNotExist) {
+    return <Redirect to="/404" />;
+  }
+
+  return (
     <>
       <h1>This is a template for blog posts.</h1>
       <p>We'll get to this once we've hooked up Firebase!</p>
     </>
-  ) : (
-    <Redirect to="/404" />
   );
 };
 
